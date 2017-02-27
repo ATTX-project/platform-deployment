4e767d62e9ec8652018bdc4d45b196ff82830157
Stefan Negru <stefan.negru@helsinki.fi>
2017-02-27 09:05:37 +0200

    ATTX-project/project-management#75 start, do tests, remove images

--- pd-feature-tests/runTests.sh
+++ pd-feature-tests/runTests.sh
@@ -3,18 +3,15 @@
 : ${SLEEP_LENGTH:=2}
 
 wait_for() {
-  echo Waiting for $1 to listen on $2... >> /tmp/log  
+  echo Waiting for $1 to listen on $2... >> /tmp/log
   while ! nc -z $1 $2; do echo sleeping >> /tmp/log ; sleep $SLEEP_LENGTH; done
 }
-
-
 
 wait_for "fuseki" "3030"
 wait_for "essiren" "9200"
 wait_for "essiren" "9300"
 wait_for "gmapi" "4302"
 wait_for "wfapi" "4301"
-wait for "frontend" "8080"
+wait_for "frontend" "8080"
 
-gradle -b /tmp/build.test.gradle --offline test
-
+gradle -b build.gradle --offline test
