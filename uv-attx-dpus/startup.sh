32d79de888aef127fa433ff9da8272b31c3357fa
Stefan Negru <stefan.negru@helsinki.fi>
2017-03-09 16:45:59 +0200

    ATTX-project/workflow-component#44 add missing dpu also fixes the pd-feature-tests

--- uv-attx-dpus/startup.sh
+++ uv-attx-dpus/startup.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+
+# Wait for MySQL
+: ${SLEEP_LENGTH:=2}
+
+wait_for() {
+  echo Waiting for $1 to listen on $2... >> /tmp/log
+  while ! nc -z $1 $2; do echo sleeping >> /tmp/log ; sleep $SLEEP_LENGTH; done
+}
+
+wait_for "mysql" "3306"
+
 # Wait till Tomcat startup has finished and webapps are started (max 3 minutes)
 i=0
 until $(curl --output /dev/null --silent --head --fail --user $MASTER_USER:$MASTER_PASSWORD http://frontend:8080/master/api/1/pipelines) || [ "$i" -gt 36 ]; do
