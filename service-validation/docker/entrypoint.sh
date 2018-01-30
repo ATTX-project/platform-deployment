c284c74ce6aae2eb83c4a9654b0086bfd528a1ba
blankdots <stefan@blankdots.com>
2018-01-30 15:23:59 +0200

    adding docker image for shacl service

--- service-validation/docker/entrypoint.sh
+++ service-validation/docker/entrypoint.sh
@@ -17,7 +17,7 @@ fi
 # If GOSU_USER environment variable set to something other than 0:0 (root:root),
 # become user:group set within and exec command passed in args
 if [ "$GOSU_USER" != "0:0" ]; then
-    exec gosu $GOSU_USER java -jar /app/rmlservice.jar "$@"    
+    exec gosu $GOSU_USER /bin/bash -c /app/validation-service-1.0.0-SNAPSHOT/bin/validation-service "$@"
 fi
 
 # If GOSU_USER was 0:0 exec command passed in args without gosu (assume already root)
