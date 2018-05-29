c606c580b4f55582ee1588cc4d6bcbc855185de2
jkesanie <jkesanie@lm5-hulib-1.hulib.helsinki.fi>
2018-05-29 13:06:45 +0300

    reverting back

--- service-ontology/docker/entrypoint.sh
+++ service-ontology/docker/entrypoint.sh
@@ -17,7 +17,7 @@ fi
 # If GOSU_USER environment variable set to something other than 0:0 (root:root),
 # become user:group set within and exec command passed in args
 if [ "$GOSU_USER" != "0:0" ]; then
-    exec gosu $GOSU_USER java -jar /app/ontology-service-1.0-SNAPSHOT.jar "$@"
+    exec gosu $GOSU_USER supervisord -c /etc/supervisor/supervisord.conf "$@"
 fi
 
 # If GOSU_USER was 0:0 exec command passed in args without gosu (assume already root)
